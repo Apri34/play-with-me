3d00eb40860ef84f95d481e4466df8ac04b58725
andreaspribitzer <andreas.pribitzer@outlook.at>
2021-08-08 19:33:47 +0200

    Zooming

--- lib/main.dart
+++ lib/main.dart
@@ -41,11 +41,12 @@ class _PlayWithMeState extends State<PlayWithMe> with TickerProviderStateMixin {
   );
 
   late final Animation<double> flipAnimation =
-      Tween<double>(begin: 0, end: 4 * pi).animate(
+  Tween<double>(begin: 0, end: 4 * pi).animate(
     CurvedAnimation(parent: _flipController, curve: Curves.linear),
-  )..addListener(() {
-          setState(() {});
-        });
+  )
+    ..addListener(() {
+      setState(() {});
+    });
 
   late double size;
   Offset? gravityBegin;
@@ -53,6 +54,7 @@ class _PlayWithMeState extends State<PlayWithMe> with TickerProviderStateMixin {
   Offset? position;
   bool gravity = false;
   List<Sprinkle> sprinkles = [];
+  double initialSize = 100;
 
   @override
   void initState() {
@@ -73,98 +75,127 @@ class _PlayWithMeState extends State<PlayWithMe> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          ...sprinkles,
-          Positioned(
-            left: position?.dx ??
-                MediaQuery.of(context).size.width / 2 - size / 2,
-            top: position?.dy ??
-                MediaQuery.of(context).size.height / 2 - size / 2,
-            child: Draggable(
-              feedback: Container(),
-              child: Transform(
-                alignment: FractionalOffset.center,
-                transform: Matrix4.identity()
-                  ..setEntry(3, 2, 0.005)
-                  ..rotateY(flipAnimation.value),
-                child: Toy(size: size),
+    return GestureDetector(
+      onScaleStart: (s) => initialSize = size,
+      onScaleUpdate: (s) {
+        double scale = s.scale;
+        if (scale > 1 && scale * initialSize <= maxSize) {
+          setState(() {
+            size = initialSize * scale;
+          });
+        } else if (scale < 1 && scale * initialSize >= minSize) {
+          setState(() {
+            size = initialSize * scale;
+          });
+        }
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            ...sprinkles,
+            Positioned(
+              left: position?.dx ??
+                  MediaQuery
+                      .of(context)
+                      .size
+                      .width / 2 - size / 2,
+              top: position?.dy ??
+                  MediaQuery
+                      .of(context)
+                      .size
+                      .height / 2 - size / 2,
+              child: Draggable(
+                feedback: Container(),
+                child: Transform(
+                  alignment: FractionalOffset.center,
+                  transform: Matrix4.identity()
+                    ..setEntry(3, 2, 0.005)
+                    ..rotateY(flipAnimation.value),
+                  child: Toy(
+                    size: size
+                  ),
+                ),
+                onDraggableCanceled: (velocity, offset) {
+                  setState(() {
+                    position = offset;
+                  });
+                },
+                onDragUpdate: (dragUpdateDetails) {
+                  setState(() {
+                    position = Offset(
+                      dragUpdateDetails.globalPosition.dx - size / 2,
+                      dragUpdateDetails.globalPosition.dy - size / 2,
+                    );
+                  });
+                },
+                onDragEnd: (dragEndDetails) {
+                  if (gravity) _runGravityAnimation(context);
+                },
               ),
-              onDraggableCanceled: (velocity, offset) {
-                setState(() {
-                  position = offset;
-                });
-              },
-              onDragUpdate: (dragUpdateDetails) {
-                setState(() {
-                  position = Offset(
-                    dragUpdateDetails.globalPosition.dx - size / 2,
-                    dragUpdateDetails.globalPosition.dy - size / 2,
-                  );
-                });
-              },
-              onDragEnd: (dragEndDetails) {
-                if (gravity) _runGravityAnimation(context);
-              },
             ),
-          ),
-          Positioned(
-            bottom: 50,
-            left: 20,
-            right: 20,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text("Gravity"),
-                    SizedBox(
-                      width: 6,
-                    ),
-                    Switch(
-                      value: gravity,
-                      onChanged: (value) {
-                        gravity = !gravity;
-                        if (gravity) {
-                          _runGravityAnimation(context);
-                        }
-                      },
-                    ),
-                  ],
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    _flipController
-                        .forward()
-                        .then((_) => _flipController.reset());
-                  },
-                  child: Text("Flip"),
-                ),
-                Slider(
-                  value: size,
-                  min: minSize,
-                  max: maxSize,
-                  onChanged: (value) {
-                    setState(() {
-                      size = value;
-                    });
-                  },
-                ),
-              ],
+            Positioned(
+              bottom: 50,
+              left: 20,
+              right: 20,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text("Gravity"),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Switch(
+                        value: gravity,
+                        onChanged: (value) {
+                          gravity = !gravity;
+                          if (gravity) {
+                            _runGravityAnimation(context);
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _flipController
+                          .forward()
+                          .then((_) => _flipController.reset());
+                    },
+                    child: Text("Flip"),
+                  ),
+                  Slider(
+                    value: size,
+                    min: minSize,
+                    max: maxSize,
+                    onChanged: (value) {
+                      setState(() {
+                        size = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
   _runGravityAnimation(BuildContext context) {
     final Animation<Offset> gravityAnimation = Tween<Offset>(
-            begin: position,
-            end: Offset(MediaQuery.of(context).size.width / 2 - size / 2,
-                MediaQuery.of(context).size.height / 2 - size / 2))
+        begin: position,
+        end: Offset(MediaQuery
+            .of(context)
+            .size
+            .width / 2 - size / 2,
+            MediaQuery
+                .of(context)
+                .size
+                .height / 2 - size / 2))
         .animate(
       CurvedAnimation(parent: _gravityController, curve: Curves.linear),
     );
